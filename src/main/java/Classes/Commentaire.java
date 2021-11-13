bc2789dd08da049ba963974832f5d3a00f90c91e
MartinMoguet <91967515+MartinMoguet@users.noreply.github.com>
2021-11-13 12:04:40 +0100

    création classe Commentaire

--- src/main/java/Classes/Commentaire.java
+++ src/main/java/Classes/Commentaire.java
@@ -4,4 +4,44 @@ public class Commentaire {
     public int id;
     public String contenu;
     public String date;
+    public Oeuvre Oeuvre;
+
+    public Commentaire(int id, String contenu, String date, Classes.Oeuvre oeuvre) {
+        this.id = id;
+        this.contenu = contenu;
+        this.date = date;
+        Oeuvre = oeuvre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContenu() {
+        return contenu;
+    }
+
+    public void setContenu(String contenu) {
+        this.contenu = contenu;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Classes.Oeuvre getOeuvre() {
+        return Oeuvre;
+    }
+
+    public void setOeuvre(Classes.Oeuvre oeuvre) {
+        Oeuvre = oeuvre;
+    }
 }
