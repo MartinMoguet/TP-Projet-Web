b2a721e3afeb78e8b2a85fa9a1b96928a46e8efd
MartinMoguet <91967515+MartinMoguet@users.noreply.github.com>
2021-11-13 11:31:10 +0100

    first commit

---
+++ src/test/java/com/example/Projet/Spring/Boot/ProjetSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projet.Spring.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
