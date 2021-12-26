66b95f71cc0ceec414d000e5df786d079579e00e
khabizard <khalilobedjaoui@gmail.com>
2021-12-26 14:02:42 +0100

    Mise en place de la récupération du nom d'un morceau

--- src/main/java/com/example/Projet/Spring/Boot/Controllers/MorceauController.java
+++ src/main/java/com/example/Projet/Spring/Boot/Controllers/MorceauController.java
@@ -5,6 +5,7 @@ import com.example.Projet.Spring.Boot.Classes.Morceau;
 import com.example.Projet.Spring.Boot.Repositories.CompositeurRepository;
 import com.example.Projet.Spring.Boot.Repositories.MorceauRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -27,32 +28,17 @@ public class MorceauController {
         String compo = String.valueOf(m.getCompositeur().getNomCompositeur());
         List<Compositeur> compositeurList = compositeurRepository.findAll();
         morceauRepository.save(m);
-        /*System.out.println(compo);
-        boolean test = compo.equals("Chopin");
-        System.out.println(test);
-        if ("Chopin" == "Chopin"){
-        System.out.println(m.getCompositeur().getNomCompositeur());
-        System.out.println(m.getCompositeur().getPrenomCompositeur());
-        System.out.println(compositeurList.get(0).getNomCompositeur());
-        System.out.println(compositeurList.get(0).getPrenomCompositeur());}*/
 
         compositeurList.forEach(compositeur -> {
-                    /*System.out.println(compositeur.getPrenomCompositeur());
-                    System.out.println(compositeur.getNomCompositeur());*/
-                    if (m.getCompositeur().getNomCompositeur().equals(compositeur.getNomCompositeur()) && m.getCompositeur().getPrenomCompositeur().equals(compositeur.getPrenomCompositeur()) ) {
-                        System.out.println(m.getCompositeur().getId());
-                        Compositeur compositeurAEnlever = m.getCompositeur();
-                        //compositeurRepository.delete(compositeurAEnlever);
-                        m.setCompositeur(compositeur);
-                        //morceauRepository.save(m);
-                        //compositeurRepository.deleteById(compositeurAEnlever.getId());
-                        System.out.println(compositeurAEnlever.getId());
-                        compositeur.getMorceauList().add(m.getNom());
-                        morceauRepository.save(m);
-                        compositeurRepository.deleteById(compositeurAEnlever.getId());
-
-
-                    }});
+            if (m.getCompositeur().getNomCompositeur().equals(compositeur.getNomCompositeur()) && m.getCompositeur().getPrenomCompositeur().equals(compositeur.getPrenomCompositeur()) ) {
+                System.out.println(m.getCompositeur().getId());
+                Compositeur compositeurAEnlever = m.getCompositeur();
+                m.setCompositeur(compositeur);
+                System.out.println(compositeurAEnlever.getId());
+                compositeur.getMorceauList().add(m.getNom());
+                morceauRepository.save(m);
+                compositeurRepository.deleteById(compositeurAEnlever.getId());
+            }});
     }
 
     @GET
@@ -72,4 +58,9 @@ public class MorceauController {
     @DELETE
     @Path("/{removeAllMorceau}")
     public void removeAllMorceau(){ morceauRepository.deleteAll();}
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("nom/{nom}")
+    public String getMoceauByNom(@PathParam("nom") String nom){return morceauRepository.findMorceauByNom(nom).getNom();}
 }
--- src/main/java/com/example/Projet/Spring/Boot/Repositories/MorceauRepository.java
+++ src/main/java/com/example/Projet/Spring/Boot/Repositories/MorceauRepository.java
@@ -4,4 +4,9 @@ import com.example.Projet.Spring.Boot.Classes.Morceau;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MorceauRepository extends JpaRepository<Morceau, Long> {
+    Morceau findMorceauByNomEquals(String nom);
+    Morceau findMorceauByNom(String nom);
+
+
+
 }
